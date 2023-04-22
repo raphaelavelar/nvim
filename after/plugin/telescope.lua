824c5b3f93265d29caa265751c2206928b7799db
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2023-04-22 21:54:00 +0000

    Add new keymapping and format files

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,35 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+
+vim.keymap.set('n', 'ff', function()
+    builtin.find_files({
+        hidden = true,
+        no_ignore = true,
+        no_ignore_parent = true
+    })
+end, {})
+
+vim.keymap.set('n', 'gf', function()
+    builtin.git_files({
+        show_untracked = true
+    })
+end, {})
+
+vim.keymap.set('n', 'gc', function()
+    builtin.git_commits({
+        show_untracked = true
+    })
+end, {})
+
+vim.keymap.set('n', 'b', builtin.buffers, {})
+
+vim.keymap.set('n', 'r', builtin.lsp_references, {})
+
+vim.keymap.set('n', 'gi', function()
+    builtin.lsp_implementations({ jump_type = "tab" })
+end, {})
+
+vim.keymap.set('n', 'ls', function()
+    builtin.lsp_document_symbols({ show_line = true })
+end, {})
+
+vim.keymap.set('n', 'D', builtin.diagnostics, {})
--- after/plugin/tokyonight.lua
+++ after/plugin/tokyonight.lua
@@ -1,19 +1,19 @@
-vim.cmd[[colorscheme tokyonight]]
+vim.cmd [[colorscheme tokyonight]]
 
 require("tokyonight").setup({
-  style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-  transparent = false,
-  terminal_colors = true,
-  styles = {
-    comments = { italic = true },
-    keywords = { italic = true },
-    functions = {},
-    variables = {},
-    sidebars = "dark",
-    floats = "dark",
-  },
-  sidebars = { "qf", "help" },
-  hide_inactive_statusline = false,
-  dim_inactive = false, 
-  lualine_bold = false,
+    style = "storm",
+    transparent = false,
+    terminal_colors = true,
+    styles = {
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        sidebars = "dark",
+        floats = "dark",
+    },
+    sidebars = { "qf", "help" },
+    hide_inactive_statusline = false,
+    dim_inactive = false,
+    lualine_bold = false,
 })
