bb5b9bb496566c1260f7e24c3b2094157025a137
Raphael <74843388+raphaelavelar@users.noreply.github.com>
2023-05-12 20:55:53 +0000

    Update command mapping

--- lua/rpl/packer.lua
+++ lua/rpl/packer.lua
@@ -5,7 +5,9 @@ return require('packer').startup(function(use)
 
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        requires = { { 'nvim-lua/plenary.nvim' } }
+        requires = {
+            { 'nvim-lua/plenary.nvim' },
+        }
     }
 
     use {
